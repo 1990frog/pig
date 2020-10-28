99aaad4a08142e7ae5e285c8287bb644b0ad1207
lianglele <lianglele@clinbrain.com>
2020-10-28 10:31:17 +0800

    解析cron日期格式类

--- etl-base/etl-base-biz/src/main/java/com/clinbrain/dip/strategy/bean/CronScheduleModelDto.java
+++ etl-base/etl-base-biz/src/main/java/com/clinbrain/dip/strategy/bean/CronScheduleModelDto.java
@@ -17,14 +17,33 @@ public class CronScheduleModelDto implements Serializable {
 	 * 2  -> 每月
 	 * 3  -> 每周
 	 * 4  ->间隔（每隔2个小时，每隔30分钟）
+	 * 5 -> 周期 必须有开始和结束时间
 	 */
 	Integer jobType;
+
+	/**
+	 * 年周期
+	 */
+	Integer[] dayOfYears;
 
 	/**一周的哪几天*/
 	Integer[] dayOfWeeks;
 
 	/**一个月的哪几天*/
 	Integer[] dayOfMonths;
+
+	/**
+	 * 开始结束周期 时
+	 */
+	Integer[] startOfEndHours;
+	/**
+	 * 开始结束周期 日
+	 */
+	Integer[] days;
+	/**
+	 * 开始结束周期 分
+	 */
+	Integer[] startOfEndMinutes;
 
 	/**秒  */
 	Integer second;
@@ -34,4 +53,15 @@ public class CronScheduleModelDto implements Serializable {
 
 	/**时  */
 	Integer hour;
+
+	/**日  每天执行 间隔1天就是每天都执行，2天是间隔一天 2--3 */
+	Integer day;
+
+	/**月  */
+	Integer month;
+
+	/**周  */
+	Integer week;
+
+
 }

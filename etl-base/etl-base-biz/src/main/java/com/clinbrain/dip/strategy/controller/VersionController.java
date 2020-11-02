ebdc4fd58d60f384de2f285928cccc85b837d804
lianglele <lianglele@clinbrain.com>
2020-11-02 09:25:04 +0800

    版本查询功能实现类

--- etl-base/etl-base-biz/src/main/java/com/clinbrain/dip/strategy/controller/VersionController.java
+++ etl-base/etl-base-biz/src/main/java/com/clinbrain/dip/strategy/controller/VersionController.java
@@ -2,30 +2,14 @@ package com.clinbrain.dip.strategy.controller;
 
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.clinbrain.dip.multirequestbody.MultiRequestBody;
-import com.clinbrain.dip.strategy.bean.PackageInfo;
-import com.clinbrain.dip.strategy.bean.SystemConnectionCodeVO;
-import com.clinbrain.dip.strategy.entity.Template;
-import com.clinbrain.dip.strategy.service.TemplateService;
 import com.clinbrain.dip.strategy.service.VersionService;
-import com.google.common.collect.Lists;
 import com.pig4cloud.pig.common.core.util.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * (TTemplet)表控制层

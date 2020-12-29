34e7e5abb6bd21745f1e76f74fcdd0848919967a
liaopan <liaopan@clinbrain.com>
2020-12-29 15:11:41 +0800

    1， 添加每天执行清除任务执行日志定时任务

--- etl-base/etl-base-biz/src/main/java/com/clinbrain/dip/schedule/EtlLogClearScheduler.java
+++ etl-base/etl-base-biz/src/main/java/com/clinbrain/dip/schedule/EtlLogClearScheduler.java
@@ -1,5 +1,10 @@
 package com.clinbrain.dip.schedule;
 
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUnit;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.date.LocalDateTimeUtil;
+import cn.hutool.core.io.FileUtil;
 import com.clinbrain.dip.common.DefineLogbackDir;
 import com.clinbrain.dip.common.DipConfig;
 import com.clinbrain.dip.util.LogBackUtil;
@@ -7,6 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.stream.Stream;
 
 /**
  * Created by Liaopan on 2020-12-09.
@@ -16,11 +23,28 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class EtlLogClearScheduler {
 
-	@Scheduled(cron = " 0 0/2 15 1/1 * ? ")
+	/**
+	 * 每天0点执行清除etl log文件
+	 */
+	@Scheduled(cron = " 0 0 0 * * ? ")
 	public void clearLog() {
 		final DipConfig configInstance = DipConfig.getConfigInstance();
 		final String logbackDir = configInstance.getProperty("logback.dir", "/logs");
-		final String maxHistoryStr = configInstance.getProperty("logback.maxHistory", "30");
-		System.out.println(logbackDir);
+		final Integer maxHistory = Integer.parseInt(configInstance.getProperty("logback.maxHistory", "60"));
+		log.info("准备清除etl系统运行日志，日志保存路径: {}, 最大保存时间(天)：{}", logbackDir, maxHistory);
+		final DateTime currentDate = DateUtil.date();
+		Stream.of(FileUtil.ls(logbackDir)).forEach(file -> {
+			final DateTime fileDate = DateUtil.parse(file.getName());
+			if(DateUtil.between(currentDate, fileDate , DateUnit.DAY) > maxHistory) {
+				log.info("准备删除{}目录下的日志文件", file.getPath());
+				try {
+					FileUtil.del(file.getPath());
+				}catch (Exception e) {
+					log.error("删除文件失败{}", file.getPath() , e);
+				}
+			}
+		});
+
 	}
+
 }

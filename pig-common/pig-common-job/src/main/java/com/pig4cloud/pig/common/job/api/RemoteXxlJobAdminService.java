a017ae6ebf52e2d3b39031b77f77955f83c9a53e
caijingquan <caijingquan@clinbrain.com>
2022-08-04 17:39:05 +0800

    xxljob api

--- pig-common/pig-common-job/src/main/java/com/pig4cloud/pig/common/job/api/RemoteXxlJobAdminService.java
+++ pig-common/pig-common-job/src/main/java/com/pig4cloud/pig/common/job/api/RemoteXxlJobAdminService.java
@@ -1,36 +1,37 @@
 package com.pig4cloud.pig.common.job.api;
 
-import com.pig4cloud.pig.common.core.constant.ServiceNameConstants;
-import com.pig4cloud.pig.common.job.api.fallback.RemoteXxlJobAdminServiceFallbackImpl;
 import com.xxl.job.core.biz.model.ReturnT;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.cloud.openfeign.SpringQueryMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@FeignClient(contextId = "remoteXxlJobAdminService", value = ServiceNameConstants.XXL_JOB_ADMIN
-		, fallbackFactory = RemoteXxlJobAdminServiceFallbackImpl.class)
+@FeignClient(name = "${xxl.job.admin.addresses:http://pig-xxl-job-admin}")
 public interface RemoteXxlJobAdminService {
 
-	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	@RequestMapping(value = "/xxl-job-admin/jobinfo/add", method = RequestMethod.POST)
 	@ResponseBody
-	ReturnT<String> add(XxlJobInfo jobInfo);
+	ReturnT<String> add(@SpringQueryMap XxlJobInfo jobInfo);
 
-	@RequestMapping(value = "/update", method = RequestMethod.POST)
+	@RequestMapping(value = "/xxl-job-admin/jobinfo/update", method = RequestMethod.POST)
 	@ResponseBody
-	ReturnT<String> update(XxlJobInfo jobInfo);
+	ReturnT<String> update(@SpringQueryMap XxlJobInfo jobInfo);
 
-	@RequestMapping(value = "/remove", method = RequestMethod.POST)
+	@RequestMapping(value = "/xxl-job-admin/jobinfo/remove", method = RequestMethod.POST)
 	@ResponseBody
 	ReturnT<String> remove(@RequestParam("id") int id);
 
-	@RequestMapping(value = "/stop", method = RequestMethod.POST)
+	@RequestMapping(value = "/xxl-job-admin/jobinfo/stop", method = RequestMethod.POST)
 	@ResponseBody
 	ReturnT<String> pause(@RequestParam("id") Integer id);
 
-	@RequestMapping(value = "/start", method = RequestMethod.POST)
+	@RequestMapping(value = "/xxl-job-admin/jobinfo/start", method = RequestMethod.POST)
 	@ResponseBody
 	ReturnT<String> start(@RequestParam("id") Integer id);
 
-	@RequestMapping(value = "/trigger", method = RequestMethod.POST)
+	@RequestMapping(value = "/xxl-job-admin/jobinfo/trigger", method = RequestMethod.POST)
 	@ResponseBody
 	ReturnT<String> triggerJob(@RequestParam("id") int id, @RequestParam("executorParam") String executorParam, @RequestParam("addressList") String addressList);
 

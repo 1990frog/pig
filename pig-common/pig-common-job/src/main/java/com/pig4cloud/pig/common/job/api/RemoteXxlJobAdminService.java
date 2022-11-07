31f117e0198e0c9de318692119ab6fa88878439b
1990frog <1990frog@gmail.com>
2022-11-07 16:00:08 +0800

    Changes

--- pig-common/pig-common-job/src/main/java/com/pig4cloud/pig/common/job/api/RemoteXxlJobAdminService.java
+++ pig-common/pig-common-job/src/main/java/com/pig4cloud/pig/common/job/api/RemoteXxlJobAdminService.java
@@ -8,29 +8,69 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author caijingquan@clinbrain.com
+ * @since 2022/11/7
+ */
 @FeignClient(name = "${xxl.job.admin.addresses:http://pig-xxl-job-admin}")
 public interface RemoteXxlJobAdminService {
 
+	/**
+	 *
+	 * @param jobInfo
+	 * @return
+	 */
 	@RequestMapping(value = "/xxl-job-admin/jobinfo/add", method = RequestMethod.POST)
 	@ResponseBody
 	ReturnT<String> add(@SpringQueryMap XxlJobInfo jobInfo);
 
+	/**
+	 *
+	 * @param jobInfo
+	 * @return
+	 */
 	@RequestMapping(value = "/xxl-job-admin/jobinfo/update", method = RequestMethod.POST)
 	@ResponseBody
 	ReturnT<String> update(@SpringQueryMap XxlJobInfo jobInfo);
 
+	/**
+	 *
+	 * @param id
+	 * @return
+	 */
 	@RequestMapping(value = "/xxl-job-admin/jobinfo/remove", method = RequestMethod.POST)
 	@ResponseBody
 	ReturnT<String> remove(@RequestParam("id") int id);
 
+	/**
+	 *
+	 * @param id
+	 * @return
+	 */
 	@RequestMapping(value = "/xxl-job-admin/jobinfo/stop", method = RequestMethod.POST)
 	@ResponseBody
 	ReturnT<String> pause(@RequestParam("id") Integer id);
 
+	/**
+	 *
+	 * @param id
+	 * @return
+	 */
 	@RequestMapping(value = "/xxl-job-admin/jobinfo/start", method = RequestMethod.POST)
 	@ResponseBody
 	ReturnT<String> start(@RequestParam("id") Integer id);
 
+	/**
+	 *
+	 * @param id
+	 * @param executorParam
+	 * @param addressList
+	 * @return
+	 */
 	@RequestMapping(value = "/xxl-job-admin/jobinfo/trigger", method = RequestMethod.POST)
 	@ResponseBody
 	ReturnT<String> triggerJob(@RequestParam("id") int id, @RequestParam("executorParam") String executorParam, @RequestParam("addressList") String addressList);
